1cf6bf57c0317bc580cd516fc991758492ff5035
Iulian Dragos <jaguarul@gmail.com>
2017-04-03 18:59:16 +0200

    Add Verse companion object.

--- src/main/scala/sparktutorial/util/Verse.scala
+++ src/main/scala/sparktutorial/util/Verse.scala
@@ -9,3 +9,19 @@ package sparktutorial.util
  * We use a case class to define the schema, as required by Spark SQL.
  */
 case class Verse(book: String, chapter: Int, verse: Int, text: String)
+
+object Verse {
+  def fromLine(line: String): Option[Verse] = {
+    // Regex to match the fields separated by "|".
+    // Also strips the trailing "~" in the KJV file.
+    val lineRE = """^\s*([^|]+)\s*\|\s*([\d]+)\s*\|\s*([\d]+)\s*\|\s*(.*)~?\s*$""".r
+    // Use flatMap to effectively remove bad lines.
+    line match {
+      case lineRE(book, chapter, verse, text) =>
+        Some(Verse(book, chapter.toInt, verse.toInt, text))
+      case line =>
+        Console.err.println(s"Unexpected line: $line")
+        None
+    }
+  }
+}
